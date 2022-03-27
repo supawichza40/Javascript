82dd1a21b664dccbfe26792badb5bf587d2b503c
Supawichza40 <supawichza@gmail.com>
2022-03-28 00:56:35 +0100

    ready to deploy

--- DJBeautyAndThaiSpa/app.js
+++ DJBeautyAndThaiSpa/app.js
@@ -442,6 +442,7 @@ app.get("/carpark", (req, res) => {
 app.use(function (err, req, res, next) {
     res.render("djbeauty/error.ejs",{error:err})
 })
-app.listen(3000, () => {
-    console.log("Listening to port 3000")
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Listening to port ${port}`)
 })
